17cb3fcb9db7ef701886171de7aaf786878b514e
Igor Stępień <oksydan56@gmail.com>
2020-01-06 00:17:04 +0100

    Firebase added, firebase config added

---
+++ src/config/fbConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCskZ2drunB0Goge4ZsFgB2vfVDZLIcrfg",
+    authDomain: "reactshoppinglistapp.firebaseapp.com",
+    databaseURL: "https://reactshoppinglistapp.firebaseio.com",
+    projectId: "reactshoppinglistapp",
+    storageBucket: "reactshoppinglistapp.appspot.com",
+    messagingSenderId: "244901484491",
+    appId: "1:244901484491:web:9eb6623d0df20f964b484e",
+    measurementId: "G-LJEB096KM7"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;

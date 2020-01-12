a299fc9159ad63829175ff2d872e26ff0b244607
Igor Stępień <oksydan56@gmail.com>
2020-01-12 01:14:20 +0100

    Added back to list btn

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 import ShoppingListForm from '../../containers/ShoppingListForm/ShoppingListForm';
 import ShoppingList from '../../containers/ShoppingList/ShoppingList';
+import { Link } from 'react-router-dom';
+
 
 const shoppingList = () => (
     <Fragment>
+        <Link to="/">Back to list</Link>
         <ShoppingListForm />
         <ShoppingList />
     </Fragment>

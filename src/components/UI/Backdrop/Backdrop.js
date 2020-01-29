a8676b2473b1379dcef5dc33534c8f0c1cab0ac6
Igor Stępień <oksydan56@gmail.com>
2020-01-29 22:51:22 +0100

    Modal styles transfered to .scss file

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,16 +3,8 @@ import PropTypes from 'prop-types';
 
 const backdrop = (props) => (
     props.show ? <div 
+                    className="backdrop"
                     onClick={props.clicked}
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        position: 'fixed',
-                        zIndex: '100',
-                        left: 0,
-                        top: 0,
-                        backgroundColor: 'rgba(0, 0, 0, .5)'
-                    }}
                     ></div> : null
 );
 

3b30e24b76536afe07dab7908ae0eee3946b4e07
Igor Stępień <oksydan56@gmail.com>
2020-02-01 02:55:04 +0100

    Added spinner component

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const spinner = () => (
+    <div className="spinner">
+        <div className="spinner__inner">
+            <div className="spinner__elem"></div>
+            <div className="spinner__elem"></div>
+            <div className="spinner__elem"></div>
+            <div className="spinner__elem"></div>
+        </div>
+    </div>
+);
+
+
+export default spinner;
